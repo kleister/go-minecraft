d501951735c9384773ac6adddd7428a12b264134
Thomas Boerger <thomas@webhippie.de>
2018-08-15 20:05:33 +0200

    Filter versions by version constraints

--- version/versions.go
+++ version/versions.go
@@ -1,13 +1,32 @@
 package version
 
+import (
+	"github.com/mcuadros/go-version"
+)
+
 // Filter defines filter attributes for Versions.
 type Filter struct {
+	Version string
 }
 
 // Versions is simply a collection of Version.
 type Versions []Version
 
 // Filter reduces the list of Versions by given criteria.
-func (v *Versions) Filter(filter *Filter) Versions {
-	return Versions{}
+func (v Versions) Filter(filter *Filter) Versions {
+	result := Versions{}
+
+	vc := version.NewConstrainGroupFromString(filter.Version)
+
+	for _, row := range v {
+		if filter.Version != "" {
+			if !vc.Match(row.ID) {
+				continue
+			}
+		}
+
+		result = append(result, row)
+	}
+
+	return result
 }
